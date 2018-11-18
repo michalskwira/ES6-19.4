ce49d1c35d4926133b47611478f938966cb355b6
Michał Skwira <michalskwira@yahoo.com>
2018-11-18 17:31:02 +0100

    Add file, job done

---
+++ script.js
@@ -0,0 +1,30 @@
+// Zadanie I
+
+const one = 'Hello';
+const two = 'World';
+
+console.log(`${one} ${two}`);
+
+// Zadanie II
+
+const multiply = (a, b = 1) => console.log(a*b);
+
+// Zadanie III
+
+const avg = (...data) => data.reduce((prev, curr) => prev + curr) / data.length;
+
+console.log(avg(2,3,34,5,6));
+
+
+// zadanie IV
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+console.log(avg(...grades));
+
+// zadanie V
+
+const data = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , , firstName, ,lastName] = data;
+
+console.log(`${firstName} ${lastName}`);
